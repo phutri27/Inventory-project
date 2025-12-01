4e8550f9776f9c525e7252ca9a01a436ed76c778
phutri27 <triphu27@gmail.com>
2025-12-01 16:10:28 +0000

    Deployment commit

--- db/pool.js
+++ db/pool.js
@@ -2,9 +2,8 @@ const { Pool } = require("pg")
 require('dotenv').config({debug: true})
 
 module.exports = new Pool({
-    host: "localhost", 
-    user: process.env.USERNAME,
-    database: process.env.DATABASE,
-    password: process.env.PASSWORD,
-    port: 5432 
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
